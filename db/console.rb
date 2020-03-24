990e7da3fecdb1c0975f612562bbd77bb87d5658
Michael Wales <michaelwales@gmail.com>
2020-03-24 20:19:45 +0000

    who even knows anymore?

--- db/console.rb
+++ db/console.rb
@@ -1,2 +1,31 @@
 require('pry-byebug')
 require_relative('../models/property')
+
+
+
+property1 = Property.new({
+  'build' => 'Semi-Detached',
+  'year_built' => '1989',
+  'number_of_bedrooms' => '3',
+  'value' => '150000'
+  })
+
+property2 = Property.new({
+  'build' => 'Detached',
+  'year_built' => '2000',
+  'number_of_bedrooms' => '4',
+  'value' => '180000'
+  })
+
+
+property1.create()
+
+# property1.year_built = 1991
+# property1.update()
+
+# property1.delete()
+
+property1.find(3)
+
+binding.pry
+nil
